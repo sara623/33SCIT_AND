a3596f08e87707ef1b8be223a3e52acd4a6f5130
Sunyoung <ysy46331749@gmail.com>
2017-12-27 10:36:32 +0900

    - 첫번쨰 커밋을 시도한다

--- app/src/main/java/com/example/user/a33android_b/Project_Main.java
+++ app/src/main/java/com/example/user/a33android_b/Project_Main.java
@@ -2,6 +2,7 @@ package com.example.user.a33android_b;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Project_Main extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Project_Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project__main);
+    }
+
+    //연습코드 - 깃 연동
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return super.onContextItemSelected(item);
     }
 }
