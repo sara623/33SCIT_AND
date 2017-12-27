b0c114658e5f1dffc782a143615c3df84dddaf37
Sunyoung <ysy46331749@gmail.com>
2017-12-27 10:33:17 +0900

    프로젝트 시작

---
+++ app/src/main/java/com/example/user/a33android_b/Project_Main.java
@@ -0,0 +1,13 @@
+package com.example.user.a33android_b;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Project_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project__main);
+    }
+}
